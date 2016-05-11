2899c5f0e024f012afe941a6dfa6300159cb7af3
wolfskaempf <tom@wolfskaempf.de>
2016-05-11 20:23:09 +0200

    add DelegateAdmin

--- jurycore/admin.py
+++ jurycore/admin.py
@@ -7,4 +7,11 @@ from .models import Committee, Delegate, Delegation
 class CommitteeAdmin(admin.ModelAdmin):
     # The information, which should be displayed in the list of committees.
     list_filter = ["name"]
+
+class DelegateAdmin(admin.ModelAdmin):
+    # The information, which should be displayed in the list of committees.
+    list_display = ("name", "committee", "delegation", "photo")
+    list_filter = ["committee__name", "delegation__name"]
+    search_fields = ["name", "committee__name", "delegation__name", "photo"]
 admin.site.register(Committee, CommitteeAdmin)
+admin.site.register(Delegate, DelegateAdmin)
