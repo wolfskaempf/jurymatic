61eecffd0c7c80576ce792cbdfc98e811d3149eb
wolfskaempf <tom@wolfskaempf.de>
2016-05-11 20:16:55 +0200

    add Delegation model

--- jurycore/models.py
+++ jurycore/models.py
@@ -12,3 +12,10 @@ class Delegate(models.Model):
 
     def __str__(self):              # __unicode__ on Python 2
         return "(%s, %s)" % (self.committee.name, self.delegation.name)
+
+
+class Delegation(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):              # __unicode__ on Python 2
+        return self.name
