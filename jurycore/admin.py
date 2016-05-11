2dd77b1bb74525c2d38a01f895f70bc3e7cc91cd
wolfskaempf <tom@wolfskaempf.de>
2016-05-11 20:18:36 +0200

    add CommitteeAdmin

--- jurycore/admin.py
+++ jurycore/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Committee, Delegate, Delegation
+
+class CommitteeAdmin(admin.ModelAdmin):
+    # The information, which should be displayed in the list of committees.
+    list_filter = ["name"]
+admin.site.register(Committee, CommitteeAdmin)
