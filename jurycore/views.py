264b09c64e2916fee7ff62940c1a6555e40f7b72
wolfskaempf <tom@wolfskaempf.de>
2016-06-11 12:03:34 +0200

    add oder_by to several view's database requests

--- jurycore/views.py
+++ jurycore/views.py
@@ -16,7 +16,7 @@ def home(request):
 
 def committee_list(request):
     """ This view shows a list of all committees"""
-    committees = Committee.objects.all()
+    committees = Committee.objects.all().order_by("name")
 
     context = {"committees": committees}
     template = "jurycore/committee_list.html"
@@ -36,15 +36,15 @@ def delegation_show(request, pk):
     """ This view shows an individual delegation and all its delegates formatted for printing """
     delegation = Delegation.objects.get(pk=pk)
 
-    delegates = Delegate.objects.filter(delegation_id=pk)
+    delegates = Delegate.objects.filter(delegation_id=pk).order_by("committee__name")
 
-    context = {"delegation": delegation, "delegates": delegates}
+    context = {"delegation": delegation, "delegates": delegates, "delegation_show": True}
     template = "jurycore/delegation_show.html"
     return render(request, template, context)
 
 def printing_view(request):
     """ This view lists all committees and all delegates at the same time, formatted for printing """
-    committees = Committee.objects.all()
+    committees = Committee.objects.all().order_by("name")
 
     context = {"committees": committees}
     template = "jurycore/printing_view.html"
