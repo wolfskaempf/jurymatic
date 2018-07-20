61e0b755d781b75817234a70c1a96e3537c59c75
wolfskaempf <tom@wolfskaempf.de>
2018-07-20 14:39:19 +0200

    fix model string representation in python 3

--- jurycore/models.py
+++ jurycore/models.py
@@ -13,7 +13,7 @@ class Delegate(models.Model):
     delegation = models.ForeignKey("Delegation", on_delete=CASCADE)
     remarks = models.TextField(blank=True)
 
-    def __unicode__(self):  # __unicode__ on Python 2
+    def __str__(self):
         return "(%s, %s)" % (self.committee.name, self.delegation.name)
 
 
@@ -21,12 +21,12 @@ class Delegation(models.Model):
     name = models.CharField(max_length=100)
     colour = models.CharField(max_length=8, blank=True)
 
-    def __unicode__(self):  # __unicode__ on Python 2
+    def __str__(self):
         return self.name
 
 
 class Committee(models.Model):
     name = models.CharField(max_length=100)
 
-    def __unicode__(self):  # __unicode__ on Python 2
+    def __str__(self):
         return self.name
