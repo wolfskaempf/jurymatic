8b5bc9eae8380a26abd5a44d5d6772726271d5ce
wolfskaempf <tom@wolfskaempf.de>
2018-07-21 23:58:00 +0200

    add Booklet to admin

--- jurycore/admin.py
+++ jurycore/admin.py
@@ -1,14 +1,14 @@
 from django.contrib import admin
-
 # Register your models here.
 from guardian.admin import GuardedModelAdmin
 
-from .models import Committee, Delegate, Delegation
+from .models import Committee, Delegate, Delegation, Booklet
 
 
-class CommitteeAdmin(GuardedModelAdmin):
-    # The information, which should be displayed in the list of committees.
-    list_filter = ["name"]
+class BookletAdmin(GuardedModelAdmin):
+    # The information, which should be displayed in the list of booklets.
+    list_display = ["session_name", "author_name", "author_email"]
+    search_fields = ["session_name", "author_name", "author_email"]
 
 
 class DelegateAdmin(GuardedModelAdmin):
@@ -18,11 +18,17 @@ class DelegateAdmin(GuardedModelAdmin):
     search_fields = ["name", "committee__name", "delegation__name", "photo"]
 
 
+class CommitteeAdmin(GuardedModelAdmin):
+    # The information, which should be displayed in the list of committees.
+    list_filter = ["name"]
+
+
 class DelegationAdmin(GuardedModelAdmin):
     # The information, which should be displayed in the list of committees.
     list_filter = ["name"]
 
 
-admin.site.register(Committee, CommitteeAdmin)
 admin.site.register(Delegate, DelegateAdmin)
+admin.site.register(Committee, CommitteeAdmin)
 admin.site.register(Delegation, DelegationAdmin)
+admin.site.register(Booklet, BookletAdmin)
