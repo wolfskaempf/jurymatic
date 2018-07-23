dde3e69dfbe9036498997316a0dc54f50b3b32cd
wolfskaempf <tom@wolfskaempf.de>
2018-07-23 20:18:35 +0200

    refactor location of printing view

--- jurycore/urls.py
+++ jurycore/urls.py
@@ -2,29 +2,36 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 
+import jurycore.views.protected_views.booklets
 import jurycore.views.public_views
 from jurycore.views.auth import sign_up;
-from jurycore.views.protected_views import overview, booklets, delegates, committees, delegations, printing
+from jurycore.views.protected_views import overview, booklets, delegates, committees, delegations
 
 app_name = 'jurycore'
 urlpatterns = [
     path('', jurycore.views.public_views.home, name='home'),
     path('dashboard/', overview.dashboard, name='dashboard'),
+    #  Booklets
     path('booklet/create/', booklets.booklet_create, name='booklet_create'),
     path('booklet/<slug:slug>/', booklets.booklet_show, name='booklet_show'),
     path('booklet/<slug:slug>/update/', booklets.booklet_update, name='booklet_update'),
     path('booklet/<slug:slug>/share/', booklets.booklet_share, name='booklet_share'),
-    path('booklet/<slug:slug>/share/revoke/<str:username>/', booklets.booklet_revoke_access, name='booklet_revoke_access'),
+    path('booklet/<slug:slug>/share/revoke/<str:username>/', booklets.booklet_revoke_access,
+         name='booklet_revoke_access'),
     path('booklet/<slug:slug>/delete/', booklets.booklet_delete, name='booklet_delete'),
+    path('booklet/<slug:booklet>/print/', jurycore.views.protected_views.booklets.booklet_print, name='printing_view'),
+    #  Delegates
     path('booklet/<slug:booklet>/delegates/', delegates.delegate_list, name='delegate_list'),
     path('booklet/<slug:booklet>/delegate/create/', delegates.delegate_create, name='delegate_create'),
     path('booklet/<slug:booklet>/delegate/<uuid:uuid>/delete/', delegates.delegate_delete, name='delegate_delete'),
     path('booklet/<slug:booklet>/delegate/<uuid:uuid>/update/', delegates.delegate_update, name='delegate_update'),
+    #  Committees
     path('booklet/<slug:booklet>/committees/', committees.committee_list, name='committee_list'),
     path('booklet/<slug:booklet>/committee/<uuid:uuid>/', committees.committee_show, name='committee_show'),
     path('booklet/<slug:booklet>/committee/<uuid:uuid>/update/', committees.committee_update, name='committee_update'),
     path('booklet/<slug:booklet>/committee/<uuid:uuid>/delete/', committees.committee_delete, name='committee_delete'),
     path('booklet/<slug:booklet>/committee/create/', committees.committee_create, name='committee_create'),
+    #  Delegations
     path('booklet/<slug:booklet>/delegations/', delegations.delegation_list, name='delegation_list'),
     path('booklet/<slug:booklet>/delegation/<uuid:uuid>/', delegations.delegation_show, name='delegation_show'),
     path('booklet/<slug:booklet>/delegation/<uuid:uuid>/update/', delegations.delegation_update,
@@ -32,7 +39,7 @@ urlpatterns = [
     path('booklet/<slug:booklet>/delegation/<uuid:uuid>/delete/', delegations.delegation_delete,
          name='delegation_delete'),
     path('booklet/<slug:booklet>/delegation/create/', delegations.delegation_create, name='delegation_create'),
-    path('booklet/<slug:booklet>/print/', printing.printing_view, name='printing_view'),
+    #  Authentication
     path('sign_up/', sign_up.sign_up, name='sign_up'),
 ]
 
--- jurycore/views/protected_views/booklets.py
+++ jurycore/views/protected_views/booklets.py
@@ -145,3 +145,15 @@ def booklet_revoke_access(request, slug, username):
         messages.warning(request, username + ' does not exist.')
 
     return HttpResponseRedirect(reverse('jurycore:booklet_share', args=[booklet.slug]))
+
+
+@login_required()
+@permission_required_or_403('view_booklet', (Booklet, 'slug', 'booklet'))
+def booklet_print(request, booklet):
+    """ This view lists all committees and all delegates at the same time, formatted for printing """
+    booklet = Booklet.objects.get(slug=booklet)
+    committees = Committee.objects.filter(booklet=booklet).order_by("name")
+
+    context = {"committees": committees}
+    template = "jurycore/printing_view.html"
+    return render(request, template, context)
