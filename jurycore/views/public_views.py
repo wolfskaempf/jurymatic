a85a732e8b3e01a1e6c33ad0eef830f47fffc50c
wolfskaempf <tom@wolfskaempf.de>
2018-07-23 23:46:07 +0200

    add error views

--- jurycore/views/public_views.py
+++ jurycore/views/public_views.py
@@ -1,16 +1,8 @@
-import uuid
-
 from django.shortcuts import redirect, render
-
-from jurycore.models import Booklet
 
 
 def home(request):
     """ This view shows some basic information to help the user understand the software """
-    booklets = Booklet.objects.all()
-    for booklet in booklets:
-        booklet.uuid = uuid.uuid4()
-        booklet.save()
     if request.user.is_authenticated:
         return redirect('jurycore:dashboard')
     context = {}
