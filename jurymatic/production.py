b92561ca8c78a20beaaa7d05d576cf585e060674
wolfskaempf <tom@wolfskaempf.de>
2018-07-23 22:35:42 +0200

    fix configuration

--- jurymatic/production.py
+++ jurymatic/production.py
@@ -118,11 +118,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
+
+
 
 # Media file settings
 MEDIA_URL = '/media/'
