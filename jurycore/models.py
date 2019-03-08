ae96486e6daad4f32128826ed3de37d38606ed57
wolfskaempf <tom@wolfskaempf.de>
2019-03-08 14:22:41 +0100

    fix boto3 s3 compatibility issue

--- jurycore/models.py
+++ jurycore/models.py
@@ -84,8 +84,7 @@ def auto_delete_file_on_change(sender, instance, **kwargs):
 
     new_file = instance.photo
     if not old_file == new_file:
-        if os.path.isfile(old_file.path):
-            os.remove(old_file.path)
+        os.remove(old_file.path)
 
 
 class Delegation(models.Model):
